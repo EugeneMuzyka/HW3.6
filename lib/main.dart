c91ce5928faf53efd1927a998abb2a36ea2f5089
Eugene Muzyka <evgenijmuzyka@gmail.com>
2022-10-11 13:38:07 +0300

    Правки

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,11 @@ class HomePage extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
+            SvgPicture.asset(
+              "images/icons8-flutter.svg",
+              semanticsLabel:
+                  'SVG From asset folder', //вариант локальной загрузки
+            ),
             CarouselSlider(
               items: imgList
                   .map((item) => Container(
@@ -64,44 +69,44 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class Picture extends StatelessWidget {
-  //вариант локальной загрузки
-  const Picture({Key? key}) : super(key: key);
+// class Picture extends StatelessWidget {
+//   //вариант локальной загрузки
+//   const Picture({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text('Flutter Demo Home Page')),
-          body: Center(
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                SvgPicture.asset(
-                  "images/icons8-flutter.svg",
-                  semanticsLabel: 'SVG From asset folder',
-                )
-                // )
-              ],
-            ),
-          )),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+//           body: Center(
+//             child: Stack(
+//               fit: StackFit.expand,
+//               children: <Widget>[
+//                 SvgPicture.asset(
+//                   "images/icons8-flutter.svg",
+//                   semanticsLabel: 'SVG From asset folder',
+//                 )
+//                 // )
+//               ],
+//             ),
+//           )),
+//     );
+//   }
+// }
 
-class UrlImg extends StatelessWidget {
-  //вариант онлайн загрузки изображения
-  const UrlImg({Key? key}) : super(key: key);
+// class UrlImg extends StatelessWidget {
+//   //вариант онлайн загрузки изображения
+//   const UrlImg({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text('text')),
-          body: Center(
-            child:
-                Image.network("https://img.icons8.com/color/480/flutter.png"),
-          )),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           appBar: AppBar(title: const Text('text')),
+//           body: Center(
+//             child:
+//                 Image.network("https://img.icons8.com/color/480/flutter.png"),
+//           )),
+//     );
+//   }
+// }
